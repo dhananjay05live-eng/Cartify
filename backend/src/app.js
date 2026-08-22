717c8a06ab2b0f1779d9237789b8126a37cdcdeb
dhananjay05live-eng <dhananjay05live@gmail.com>
2026-08-22 21:50:52 +0530

    initial data models

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,4 +3,8 @@ import express from "express";
 const app = express();
 
 
+app.get('/home',(req,res)=>{
+ res.send("HOME")
+})
+
 export{app}
---
+++ backend/src/modules/address/address.model.js
@@ -0,0 +1,36 @@
+import mongoose, { trusted } from "mongoose"
+
+const addressSchema = new mongoose.Schema(
+    {
+        country:{
+            type:String,
+            required:true
+        },
+        state:{
+            type:String,
+            required:true
+        },
+        district:{
+            type:String,
+            required:true
+        },
+        pinCode:{
+            type:Number,
+            required:true
+        },
+        street:{
+            type:String,
+            required:true
+        },
+        landmark:{
+            type:String,
+            required:true
+        },
+        houseNO:{
+            type:String,
+            required:true
+        }
+    }
+);
+
+export const Address = mongoose.model("Address",addressSchema);
